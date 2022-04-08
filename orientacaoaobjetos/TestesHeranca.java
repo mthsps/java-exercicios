c87327271c9138343869e48c00c280831d95171f
Matheus <dmatheusps@gmail.com>
2022-04-08 11:40:20 -0300

    #18 - Polimorfismo e herança

--- orientacaoaobjetos/TestesHeranca.java
+++ orientacaoaobjetos/TestesHeranca.java
@@ -1,6 +1,22 @@
 package orientacaoaobjetos;
 
 public class TestesHeranca {
+	
+	/* 1.  Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e
+	* comportamentos (observe a tabela), utilize os seus conhecimentos e distribua as
+	* características de forma que tudo o que for comum a todos os animais fique na classe
+	* Animal: 
+	*		Mãe: Animal
+	*			Filhos: 
+	*				Cavalo: nome, idade, correr, emitir som;
+	*				Cachorro: nome, idade, correr, emitir som;
+	*				Preguica: nome, idade, correr, subir em árvores;		
+	*
+	*
+	* 2. Implemente um programa que crie os 3 tipos de animais definidos no exercício
+	*  anterior e invoque o método que emite o som de cada um de forma polimórfica, isto
+	*  é, dependente do tipo de animal. 
+	*/	
 
 	public static void main(String[] args) {
 		
